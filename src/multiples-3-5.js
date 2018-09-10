ad91763ae83fe737551bf3d981e6dfa2d9873f05
Spencer Rendon <you@example.com>
2018-09-10 14:11:08 -0400

    Coding Challenge

--- src/multiples-3-5.js
+++ src/multiples-3-5.js
@@ -1,8 +1,27 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 // Note: If the number is a multiple of both 3 and 5, only count it once.
-function solution(number){
+function solution(number) {
   // TODO: Create the solution
+
+  let sum = 0;
+
+  if(number === null){
+
+      return -1;
+  }
+
+
+  for (let i = 0; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+
+      sum = sum + i;
+
+    }
+
+  }
+  return sum;
 }
+
 
 module.exports = solution;
--- src/sticks.js
+++ src/sticks.js
@@ -17,8 +17,74 @@ stick lengths     length of cut     sticks before cut
 - - - - - 3         3                 1
 - - - - - -         done              done
 */
-function solution(arr){
+function solution(arr) {
   // TODO: Create the solution
-}
+
+  for (let k = 0; k < arr.length; k++) {//iterate array until done
+
+
+    let smallestStick = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {//get smallest stick length
+
+      if (arr[i] <= smallestStick) {
+
+        smallestStick = arr[i];
+
+      }
+
+
+    }//end of get smallest stick
+
+
+    for (let j = 0; j < arr.length; j++) {//edit array 
+
+      if (arr[j] === smallestStick) { //removes the items that are equal to the lower stick
+
+        arr.splice(j, 1)
+
+
+      }
+
+      else if (arr[j] != smallestStick) { // subtracts the smallest stick amount from the larger sticks
+
+        arr[j] = arr[j] - smallestStick
+
+      }
+
+
+    }//end of edit array
+
+
+    for (let l = 1; l < arr.length; l++) {//check elements array for similarity
+
+      if (arr[l] === arr[1 - 1]) { //if all indecies have same values return empty array
+        
+        
+      }
+
+      else{// element not similar
+        break;//exit loop
+      }
+
+
+    }// end check elements for similarity
+
+
+       if (arr.length === 1) {//if only one element left in the array
+
+      return arr;
+    }
+
+    else { // if different elements then go to interate again
+
+    }
+
+  } // end of for loop to iterate through sticks until end
+
+  return arr;
+}// end function
+
+
 
 module.exports = solution;
